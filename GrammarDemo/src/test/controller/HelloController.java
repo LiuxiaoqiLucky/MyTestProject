a41de487e8ee90a572c61d79a2b22e6ed2dd3c21
LiuxiaoqiLucky <751025925@qq.com>
2022-01-16 19:25:38 +0800

    新增測試類commit

---
+++ GrammarDemo/src/test/controller/HelloController.java
@@ -0,0 +1,13 @@
+package test.controller;
+
+/**
+ * @Desc:
+ * @Author: Administrator
+ * @Date: 2022/1/16-19:19
+ */
+public class HelloController {
+    // test
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
